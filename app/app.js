5fe380fd545e1bc6f87b866313f5741c9612da34
eagobert <eagobert@hotmail.com>
2018-02-03 20:13:53 -0500

    Change bootstrap template to angularjs and Add menu handler

---
+++ app/app.js
@@ -0,0 +1,14 @@
+const myApp = angular.module('myApp', [])
+
+
+myApp.controller("MyAppCtrl", function($scope){
+    
+    // Opens and closes the sidebar menu
+    $scope.MenuHandler = function(){$(".menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+        $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+        $(this).toggleClass("active");
+      });
+    }
+  })
