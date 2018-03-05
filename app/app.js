1ae742ea93861d2d18464fcf3336b24b5dd2aac3
eagobert <eagobert@hotmail.com>
2018-03-04 23:26:33 -0600

    Add portfolio carosel

--- app/app.js
+++ app/app.js
@@ -1,29 +1,7 @@
 const app = angular.module("app", ["firebase"]);
 
-// Begin Router
-
-
 
 //Begin Controller Functions
-app.controller("appCtrl", function ($scope, $http) {
+app.controller("appCtrl", function ($scope) {
 
-  // Opens and closes the sidebar menu
-  // $scope.menuController = function () {
-  //   $(".menu-toggle").click(function (e) {
-  //     e.preventDefault();
-  //     $("#sidebar-wrapper").toggleClass("active");
-  //     $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
-  //     $(this).toggleClass("active");
-  //   });
-  // }
-})
-
-  //Controls form data input and submission by email
-app.controller("formCtrl", function ($scope, $http, $timeout) {
-
-  $scope.formData = {};
-
-  $scope.submitForm = function(){}
-
-
-  })
+});
